debd9c5c1bb4e56735e91db0bf85321e76058672
Roman_v <vigmol@ukr.net>
2018-11-28 16:20:58 +0200

    fix classPath

--- src/main/java/com/ua/SpringDemo.java
+++ src/main/java/com/ua/SpringDemo.java
@@ -1,9 +1,6 @@
 package com.ua;
 
 import com.ua.entity.RobotT1000;
-import com.ua.impl_Sony.HeadSony;
-import com.ua.impl_Toshiba.HandToshiba;
-import com.ua.impl_Toshiba.LegToshiba;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -12,14 +9,5 @@ public class SpringDemo {
         ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
         RobotT1000 robotT1000 = (RobotT1000) context.getBean("robotT1000");
         robotT1000.dance();
-
-       /* HeadSony headSony = new HeadSony();
-        HandToshiba handToshiba = new HandToshiba();
-        LegToshiba legToshiba = new LegToshiba();
-
-        Robot robot = new Robot(headSony, handToshiba, legToshiba);
-        robot.action();*/
     }
-
-
 }
